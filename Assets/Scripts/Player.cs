0ca72933fb605cbd20ecf5083edcec9f50543bbd
ryzhovalex <thed4rkof@gmail.com>
2023-05-16 00:39:09 +0300

    feat: input system + collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,28 +5,106 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float radius = .7f;
+    [SerializeField] private float height = 2f;
+
+    private PlayerInputActions playerInputActions;
+
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
 
     private void Update() {
-        Vector2 inputDirection = new Vector2(0, 0);
+        ResolveMoving(
+            playerInputActions.Player.Move.ReadValue<Vector2>().normalized
+        );
+    }
 
-        if (Input.GetKey(KeyCode.W)) {
-            inputDirection.y++;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputDirection.x--;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputDirection.y--;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputDirection.x++;
+    private void ResolveMoving(
+        Vector2 inputDirection
+    ) {
+        Vector3 capsulePoint1 = transform.position;
+        Vector3 capsulePoint2 = transform.position + Vector3.up * height;
+        float moveDistance = Time.deltaTime * moveSpeed;
+
+        // check an initial intention to move
+        Vector3 initialMoveDirection = new Vector3(
+            inputDirection.x, 0, inputDirection.y
+        );
+        Vector3 finalMoveDirection = initialMoveDirection;
+        if (
+            !CheckMove(
+                capsulePoint1,
+                capsulePoint2,
+                initialMoveDirection,
+                moveDistance
+            )
+        ) {
+            // check x
+            finalMoveDirection = new Vector3(
+                initialMoveDirection.x,
+                0,
+                0
+            ).normalized;
+            if (
+                !CheckMove(
+                    capsulePoint1,
+                    capsulePoint2,
+                    finalMoveDirection,
+                    moveDistance
+                )
+            ) {
+                // check z
+                finalMoveDirection = new Vector3(
+                    0,
+                    0,
+                    initialMoveDirection.z
+                ).normalized;
+                if (
+                    !CheckMove(
+                        capsulePoint1,
+                        capsulePoint2,
+                        finalMoveDirection,
+                        moveDistance
+                    )
+                ) {
+                    return;
+                }
+            }
         }
 
-        inputDirection = inputDirection.normalized;
+        ApplyMove(
+            finalMoveDirection,
+            moveDistance
+        );
+    }
 
-        Vector3 moveDirection = new Vector3(inputDirection.x, 0f, inputDirection.y);
+    /// <summary>
+    /// Applies a move direcion and a move distance to the current position.
+    /// </summary>
+    private void ApplyMove(
+        Vector3 moveDirection,
+        float moveDistance
+    ) {
+        transform.position += moveDirection * moveDistance;
+        transform.forward = Vector3.Slerp(
+            transform.forward, moveDirection, moveDistance
+        );
+    }
 
-        transform.position += moveDirection * Time.deltaTime * moveSpeed;
-        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    private bool CheckMove(
+        Vector3 point1,
+        Vector3 point2,
+        Vector3 moveDirection,
+        float moveDistance
+    ) {
+        return !Physics.CapsuleCast(
+            point1,
+            point2,
+            radius,
+            moveDirection,
+            moveDistance
+        );
     }
 }
